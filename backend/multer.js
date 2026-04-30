347ca8f0858f448d485f1ce460937b4a71848538
Ubaid <ubaid@swiss-te.ch>
2026-04-30 17:18:05 +0500

    used multer

--- backend/multer.js
+++ backend/multer.js
@@ -1,25 +1,15 @@
-/* const multer = require("multer");
-const path = require("path");
+const multer = require('multer');
+const path = require('path');
 
 const storage = multer.diskStorage({
   destination: function (req, res, cb) {
-    cb(null, path.join(__dirname, "./uploads"));
+    cb(null, path.join(__dirname, './uploads'));
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    const filename = file.originalname.split(".")[0];
-    cb(null, filename + "-" + uniqueSuffix + ".png");
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    const filename = file.originalname.split('.')[0];
+    cb(null, filename + '-' + uniqueSuffix + '.png');
   },
 });
 
 exports.upload = multer({ storage: storage });
- */
-
-const multer = require('multer');
-
-const storage = multer.memoryStorage(); // 🔥 store in RAM, not disk
-
-exports.upload = multer({
-  storage: storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // optional (5MB)
-});
