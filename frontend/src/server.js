3aa15fc7ea757ab3bccceec92384d9dd357652f6
Ubaid <ubaid@swiss-te.ch>
2026-04-30 16:17:13 +0500

    cloudinary used for images uploading

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -4,6 +4,6 @@ export const server =
 export const backend_url =
   'https://online-shop-production-ef53.up.railway.app/';
 
-/* export const server = "http://localhost:8000/api/v2";
+/* export const server = 'http://localhost:8000/api/v2';
 
-export const backend_url = "http://localhost:8000/"; */
+export const backend_url = 'http://localhost:8000/'; */
--- frontend/src/static/data.js
+++ frontend/src/static/data.js
@@ -188,78 +188,6 @@ export const brandingData = [
 ];
 
 // categories data
-export const _categoriesData = [
-  {
-    id: 1,
-    title: 'Computers and Laptops',
-    subTitle: '',
-    image_Url:
-      'https://cdn.shopify.com/s/files/1/1706/9177/products/NEWAppleMacbookProwithM1ProChip14InchLaptop2021ModelMKGQ3LL_A_16GB_1TBSSD_custommacbd.jpg?v=1659592838',
-  },
-  {
-    id: 2,
-    title: 'cosmetics and body care',
-    subTitle: '',
-    image_Url:
-      'https://indian-retailer.s3.ap-south-1.amazonaws.com/s3fs-public/2021-07/kosme1.png',
-  },
-  {
-    id: 3,
-    title: 'Accesories',
-    subTitle: '',
-    image_Url:
-      'https://img.freepik.com/free-vector/ordering-goods-online-internet-store-online-shopping-niche-e-commerce-website-mother-buying-babies-clothes-footwear-toys-infant-accessories_335657-2345.jpg?w=2000',
-  },
-  {
-    id: 4,
-    title: 'Cloths',
-    subTitle: '',
-    image_Url:
-      'https://www.shift4shop.com/2015/images/industries/clothing/clothing-apparel.png',
-  },
-  {
-    id: 5,
-    title: 'Shoes',
-    subTitle: '',
-    image_Url:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvBQPQMVNRd6TtDkGs2dCri0Y-rxKkFOiEWw&usqp=CAU',
-  },
-  {
-    id: 6,
-    title: 'Gifts',
-    subTitle: '',
-    image_Url:
-      'https://securecdn.pymnts.com/wp-content/uploads/2014/11/Gifts-Photo-700x489.jpg',
-  },
-  {
-    id: 7,
-    title: 'Pet Care',
-    subTitle: '',
-    image_Url: 'https://cdn.openpr.com/T/c/Tc15444071_g.jpg',
-  },
-  {
-    id: 8,
-    title: 'Mobile and Tablets',
-    subTitle: '',
-    image_Url:
-      'https://st-troy.mncdn.com/mnresize/1500/1500/Content/media/ProductImg/original/mpwp3tua-apple-iphone-14-256gb-mavi-mpwp3tua-637986832343472449.jpg',
-  },
-  {
-    id: 9,
-    title: 'Music and Gaming',
-    subTitle: '',
-    image_Url:
-      'https://static.vecteezy.com/system/resources/previews/011/996/555/original/3d-black-headphone-illustration-ecommerce-icon-png.png',
-  },
-  {
-    id: 10,
-    title: 'Others',
-    subTitle: '',
-    image_Url:
-      'https://searchspring.com/wp-content/uploads/2022/10/Hero-Image-Platform-Others-2.png',
-  },
-];
-
 export const categoriesData = [
   {
     id: 1,
@@ -556,7 +484,7 @@ export const productData = [
     stock: 10,
   },
   {
-    id: 1,
+    id: 3,
     category: 'Computers and Laptop',
     name: 'MacBook pro M2 chipset 256gb ssd 8gb ram space gray color with apple 1 year warranty',
     description:
@@ -647,7 +575,7 @@ export const productData = [
     stock: 10,
   },
   {
-    id: 1,
+    id: 6,
     name: 'Gaming Headphone Asus with mutiple color and free delivery',
     description:
       "Product details are a crucial part of any eCommerce website or online marketplace. These details help the potential customers to make an informed decision about the product they are interested in buying. A well-written product description can also be a powerful marketing tool that can help to increase sales.Product details typically include information about the product's features, specifications, dimensions, weight, materials, and other relevant information that can help customers to understand the product better. The product details section should also include high-quality images and videos of the product, as well as customer reviews and ratings.",
@@ -686,7 +614,7 @@ export const productData = [
     category: 'Music and Gaming',
   },
   {
-    id: 4,
+    id: 7,
     name: 'New Fashionable Watch for men 2023 with multiple colors',
     description:
       "Product details are a crucial part of any eCommerce website or online marketplace. These details help the potential customers to make an informed decision about the product they are interested in buying. A well-written product description can also be a powerful marketing tool that can help to increase sales.Product details typically include information about the product's features, specifications, dimensions, weight, materials, and other relevant information that can help customers to understand the product better. The product details section should also include high-quality images and videos of the product, as well as customer reviews and ratings.",
@@ -715,7 +643,7 @@ export const productData = [
     stock: 10,
   },
   {
-    id: 1,
+    id: 8,
     name: 'Gaming Headphone Asus with mutiple color and free delivery',
     description:
       "Product details are a crucial part of any eCommerce website or online marketplace. These details help the potential customers to make an informed decision about the product they are interested in buying. A well-written product description can also be a powerful marketing tool that can help to increase sales.Product details typically include information about the product's features, specifications, dimensions, weight, materials, and other relevant information that can help customers to understand the product better. The product details section should also include high-quality images and videos of the product, as well as customer reviews and ratings.",
@@ -753,7 +681,7 @@ export const productData = [
     stock: 10,
   },
   {
-    id: 2,
+    id: 9,
     category: 'Mobile and Tablets',
     name: 'Iphone 14 pro max 256 gb ssd and 8 gb ram silver colour',
     description:
@@ -782,7 +710,7 @@ export const productData = [
     stock: 10,
   },
   {
-    id: 1,
+    id: 10,
     category: 'Music and Gaming',
     name: 'Gaming Headphone Asus with mutiple color and free delivery',
     description:
