903dccbd5583a2f006d0ed650741bcc52faaa644
Ubaid <ubaid@swiss-te.ch>
2026-04-30 16:30:58 +0500

    cloudinary used for images uploading

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,9 +1,9 @@
-export const server =
+/* export const server =
   'https://online-shop-production-ef53.up.railway.app/api/v2';
 
 export const backend_url =
-  'https://online-shop-production-ef53.up.railway.app/';
+  'https://online-shop-production-ef53.up.railway.app/'; */
 
-/* export const server = 'http://localhost:8000/api/v2';
+export const server = 'http://localhost:8000/api/v2';
 
-export const backend_url = 'http://localhost:8000/'; */
+export const backend_url = 'http://localhost:8000/';
