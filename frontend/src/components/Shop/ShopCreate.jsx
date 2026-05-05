cc200ab6333ba3b990fc610cfe8bc526db08f4ff
Ubaid <ubaid@swiss-te.ch>
2026-05-05 15:33:59 +0500

    remove additional submit button on create shop

--- frontend/src/components/Shop/ShopCreate.jsx
+++ frontend/src/components/Shop/ShopCreate.jsx
@@ -292,15 +292,6 @@ const ShopCreate = () => {
               >
                 Submit
               </button>
-              <button
-                onClick={(e) => {
-                  handleSubmit(e);
-                }}
-                type='button'
-                className=' className="group relative w-full h-[40px] flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"'
-              >
-                Submit button
-              </button>
             </div>
 
             <div className={`${styles.noramlFlex} w-full`}>
